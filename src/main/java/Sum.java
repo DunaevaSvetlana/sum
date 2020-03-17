cb86a40b4faccc2810ba0285005171de2d34085d
DunaevaSvetlana <solnste1993@mail.ru>
2020-03-17 21:17:11 +0400

    Initial commit

---
+++ src/main/java/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[] args) {
+        int sum = 0;
+        int n = 100;
+        for (int i = 1; i < n+1; i++) {
+            sum = sum + i;
+        }
+            System.out.println("Сумма от 1 до 100 = "+ sum);
+    }
+}
